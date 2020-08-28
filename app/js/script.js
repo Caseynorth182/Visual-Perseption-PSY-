0688c93fd705c1b931694bb569af206afafd7f08
Alexander Baranov <Caseynorth182@gmail.com>
2020-08-28 17:45:56 +0300

    descktop ver

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,12 @@
 $(function() {
+    $('.feedback__inner').slick({
+        prevArrow: '<img class="leftArrow" src="../img/feedback(slider)/left.svg">',
+        nextArrow: '<img class="rightArrow" src="../img/feedback(slider)/right.svg">',
+        infinite: true,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -37,13 +37,19 @@ gulp.task("script", function() {
     return gulp
         .src([
             "node_modules/jquery/dist/jquery.js",
-            /* "node_modules/slick-carousel/slick/slick.js", */
-            /*  "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
-             "node_modules/mixitup/dist/mixitup.js",
-             "node_modules/rateyo/src/jquery.rateyo.js",
-             "node_modules/ion-rangeslider/js/ion.rangeSlider.js",
-             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js",
-             "node_modules/jquery-form-styler/dist/jquery.formstyler.js", */
+            "node_modules/slick-carousel/slick/slick.js",
+            /*   "node_modules/magnific-popup/dist/jquery.magnific-popup.js", */
+            /* "node_modules/mixitup/dist/mixitup.js", */
+            /*  "node_modules/rateyo/src/jquery.rateyo.js", */
+            /*  "node_modules/ion-rangeslider/js/ion.rangeSlider.js", */
+            /*     "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js", */
+            /*  "node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js",
+                 "node_modules/fontawesome-pro-5.8.2-web/all.js", */
+            "node_modules/wowjs/wow.js",
+            /*    "node_modules/focus-visible/dist/focus-visible.js",
+               "node_modules/paginationjs/dist/pagination.js",  */
+            /* "node_modules/swiper/swiper.js", */
+            /* "node_modules/jquery-form-styler/dist/jquery.formstyler.js",  */
         ])
         .pipe(concat("libs.min.js"))
         .pipe(uglify())
@@ -55,32 +61,34 @@ gulp.task("style", function() {
             "node_modules/slick-carousel/slick/slick.css",
             /*   "node_modules/magnific-popup/dist/magnific-popup.css", */
             "node_modules/normalize.css/normalize.css",
-            /*  "node_modules/rateyo/src/jquery.rateyo.css",
-             "node_modules/ion-rangeslider/css/ion.rangeSlider.css",
-             "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css",
-             "node_modules/jquery-form-styler/dist/jquery.formstyler.css", */
-
+            /* "node_modules/rateyo/src/jquery.rateyo.css", */
+            /* "node_modules/ion-rangeslider/css/ion.rangeSlider.css", */
+            /*    "node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css",
+               "node_modules/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css",
+               "node_modules/fontawesome-pro-5.8.2-web/all.css", */
+            "node_modules/wowjs/animate.css",
+            /*   "node_modules/paginationjs/dist/pagination.css", */
+            /* "node_modules/swiper/swiper.css", */
+            /* "node_modules/jquery-form-styler/dist/jquery.formstyler.css",  */
         ])
         .pipe(concat("libs.min.css"))
         .pipe(cssmin())
         .pipe(gulp.dest("app/css"));
 });
 
-gulp.task('html', function() {
-    return gulp.src('app/*.html')
-        .pipe(
-            browserSync.reload({
-                stream: true,
-            })
-        );
+gulp.task("html", function() {
+    return gulp.src("app/*.html").pipe(
+        browserSync.reload({
+            stream: true,
+        })
+    );
 });
-gulp.task('js', function() {
-    return gulp.src('app/js/*.js')
-        .pipe(
-            browserSync.reload({
-                stream: true,
-            })
-        );
+gulp.task("js", function() {
+    return gulp.src("app/js/*.js").pipe(
+        browserSync.reload({
+            stream: true,
+        })
+    );
 });
 
 gulp.task("browser-sync", function() {
@@ -92,11 +100,20 @@ gulp.task("browser-sync", function() {
 });
 
 gulp.task("watch", function() {
-    gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"))
-    gulp.watch("app/*.html", gulp.parallel("html"))
-    gulp.watch("app/js/*.js", gulp.parallel("js"))
-
-
+    gulp.watch("app/scss/**/*.scss", gulp.parallel("sass"));
+    gulp.watch("app/*.html", gulp.parallel("html"));
+    gulp.watch("app/js/*.js", gulp.parallel("js"));
 });
 
-gulp.task("default", gulp.parallel("style", "script", "html", "js", "sass", "watch", "browser-sync"));
+gulp.task(
+    "default",
+    gulp.parallel(
+        "style",
+        "script",
+        "html",
+        "js",
+        "sass",
+        "watch",
+        "browser-sync"
+    )
+);
